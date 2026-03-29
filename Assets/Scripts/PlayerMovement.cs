33e59d9a2d76dd4dd13260cac11f9d5ff915541f
Azert-ty <bankolechristopher91@gmail.com>
2026-03-29 18:55:43 +0100

    feat: solid player movement and dashing system with state locking

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections;
+using System.Diagnostics;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -8,17 +10,35 @@ public class PlayerMovement : MonoBehaviour
     private PlayerControls controls;
 
     private Vector2 moveDirection;
+    private Vector2 dashDirection;
+
+    private bool candash=true;
+
+   
+
 
 
     private Rigidbody2D rb2D;
 
+    private enum PlayerState {Idle,Move,Dashing}
+
+    private PlayerState _currentState;
+
     [SerializeField]
     private float moveSpeed=5;
+
+    [SerializeField]
+    private float  _dashForce=20f;
+    [SerializeField]
+    private float _dashDuration=0.5f;
+    [SerializeField]
+    private float _dashCooldown=0.9f;
 
 
     void Awake()
     {
          controls=new PlayerControls();
+         
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -32,30 +52,82 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-         moveDirection=controls.Player.Move.ReadValue<Vector2>();
-         
+        moveDirection=controls.Player.Move.ReadValue<Vector2>();
+        
     }
-    private void onMove()
+
+    IEnumerator  OnDash()
+    {
+            candash=false;
+            _currentState=PlayerState.Dashing;
+        if (dashDirection == Vector2.zero)
+        {
+            dashDirection=Vector2.left;
+        }
+            rb2D.linearVelocity=_dashForce*dashDirection;
+            yield return new WaitForSeconds(_dashDuration);
+            rb2D.linearVelocity=Vector2.zero;
+            _currentState=PlayerState.Idle;
+            yield return new WaitForSeconds(_dashCooldown);
+            candash=true;
+        
+        
+    }
+
+
+    void onMove()
     {
         moveDirection=moveDirection.normalized;
         rb2D.linearVelocity=moveDirection*moveSpeed;
+        dashDirection=moveDirection;
           
     }
 
     void OnEnable()
     {
         controls.Enable();
+        controls.Player.Dash.performed+=HandleDashInput;
+    }
+
+    private void HandleDashInput(InputAction.CallbackContext callbackContext)
+    {
+        if(candash && _currentState != PlayerState.Dashing)
+        {
+            StartCoroutine(OnDash());
+        }
     }
 
     void OnDisable()
     {
+        controls.Player.Dash.performed-=HandleDashInput;
         controls.Disable();
     }
 
+    void OnIdle()
+    {
+        rb2D.linearVelocity=Vector2.zero;
+    }
+
+    
     
 
     void FixedUpdate()
     {
-        onMove();
+
+
+        if (_currentState == PlayerState.Dashing)
+        {
+            return;
+        }
+        if (moveDirection!=Vector2.zero)
+        {
+           onMove();
+        }
+        else
+        {
+            OnIdle();
+        }
+    
+       
     }
 }
