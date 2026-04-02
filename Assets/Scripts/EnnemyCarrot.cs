01a8b8315cc466a1fd0798114a2bb5094d78a12a
Azert-ty <bankolechristopher91@gmail.com>
2026-04-02 16:10:46 +0100

    feat: enhance EnnemyCarrot attack logic with state management and firing anticipation

--- Assets/Scripts/EnnemyCarrot.cs
+++ Assets/Scripts/EnnemyCarrot.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections;
+using System.Threading;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -13,10 +14,15 @@ public class EnnemyCarrot : MonoBehaviour
     [SerializeField]
     private float _firerate=1.5f;
 
+
+    [SerializeField]
+    private int  _fireanticipation=3;
+
     private bool canShoot=true;
+    private bool _isfirsttime;
     private enum EnnemyState
     {
-        Idle,Shooting
+        Idle,Alert,Shooting
     }
 
     private EnnemyState _currentEnnemyState;
@@ -37,30 +43,59 @@ public class EnnemyCarrot : MonoBehaviour
 
     void FixedUpdate()
     {
-        if ((player.transform.position - transform.position).sqrMagnitude <_detectionRange *_detectionRange)
+       
+        // else
+        // {
+        //     _currentEnnemyState=EnnemyState.Idle;
+        // }
+
+        if(_currentEnnemyState==EnnemyState.Alert && canShoot)
         {
+            StartCoroutine(Attack());
+        }
+
+        
+    }
+
+    // IEnumerator Shoot()
+    // {
+        
+    //     canShoot=false;
+
+    //     Debug.Log("Shoot");
+    //     yield return new WaitForSeconds(_firerate);
+    //     canShoot=true;
+
+    // }
+
+    // IEnumerator AlertShoot()
+    // {
+    //     yield return new WaitForSeconds(_fireanticipation);
+        
+    // }
+
+    IEnumerator Attack()
+    {
+        canShoot=false;
+         if ((player.transform.position - transform.position).sqrMagnitude <_detectionRange *_detectionRange)
+        {
+            _currentEnnemyState=EnnemyState.Alert;
+            yield return new WaitForSeconds(_fireanticipation); 
+            _isfirsttime=true;
             _currentEnnemyState=EnnemyState.Shooting;
-            
+        }
+        if (_currentEnnemyState==EnnemyState.Shooting)
+        {
+            Debug.Log("shoot");
+            yield return new WaitForSeconds(_firerate); 
+            canShoot=true;
+             _currentEnnemyState=EnnemyState.Shooting;
         }
         else
         {
+            _isfirsttime=false;
             _currentEnnemyState=EnnemyState.Idle;
         }
-
-        if (_currentEnnemyState == EnnemyState.Shooting && canShoot==true)
-        {
-            StartCoroutine(Shoot());
-        }
-    }
-
-    IEnumerator Shoot()
-    {
-        
-        canShoot=false;
-        Debug.Log("Shoot");
-        yield return new WaitForSeconds(_firerate);
-        canShoot=true;
-
     }
 
     void OnDrawGizmosSelected()
